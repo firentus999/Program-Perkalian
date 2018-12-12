06efe355c9772575393069aea336bb705b6eac68
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-12 09:24:20 +0700

    Program

---
+++ Program Perkalian.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    float hasil;
+    cout<<"masukan bilangan:";
+    cin>>x;
+    cout<<"masukan bilangan:";
+    cin>>y;
+    hasil=x/y;
+    cout<<"hasil pembagian adalah"<<hasil;
+    return 0;
+}
